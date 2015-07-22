e5c3f70ed653e1a8a733e70c835eba5bba640999
sa-lo-mon <salomon85r@gmail.com>
2015-07-22 06:10:00 +0300

    standard login

--- web/app.js
+++ web/app.js
@@ -5,12 +5,15 @@ var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 
 console.log('start');
+
 // Connection URL
 var url = 'mongodb://shopper:idc2015@ds053300.mongolab.com:53300/shopperdb';
+
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server");
     db.close();
 });
+
 console.log('end');
