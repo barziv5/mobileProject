8b7eb822ebe073a3064428201642692972dafd10
barziv5 <barziv4@gmail.com>
2015-07-23 13:40:07 +0300

    express hello world

--- web/app.js
+++ web/app.js
@@ -4,16 +4,31 @@
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
 console.log('start');
 
 // Connection URL
-var url = 'mongodb://shopper:idc2015@ds053300.mongolab.com:53300/shopperdb';
+/*var url = 'mongodb://shopper:idc2015@ds053300.mongolab.com:53300/shopperdb';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server");
     db.close();
-});
+});*/
 
 console.log('end');
